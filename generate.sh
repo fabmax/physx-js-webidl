213e0cacb5c00f1fbe05e5bb2a38694d4d96a5a6
fabmax <fabmax.thiele@gmail.com>
2023-12-15 18:55:10 +0100

    fix failing build caused by packman dependency issues

--- generate.sh
+++ generate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# make sure packman is up-to-date to avoid failing build:
+# https://github.com/NVIDIA-Omniverse/PhysX/discussions/230
+./PhysX/physx/buildtools/packman/packman update -y
+
 cd ./PhysX/physx
 rm -rf compiler/emscripten-*
 ./generate_projects.sh emscripten
